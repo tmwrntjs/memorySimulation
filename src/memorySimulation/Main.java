9cbc20f69e456d6e015eb5bc98f50ee7bb66e7ff
tmwrntjs <timdaryl1@hotmail.com>
2017-03-30 22:17:52 -0500

    Commit

--- src/memorySimulation/Main.java
+++ src/memorySimulation/Main.java
@@ -5,19 +5,28 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+/**
+ * 
+ * @author Tim Warntjes, Andrew Dulmes
+ * 
+ * Main class that reads and does a memory simulation 
+ *
+ */
 public class Main {
 
 	public static boolean quiet;
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		quiet=false;
+		quiet=true;
 		ArrayList<Page> pagesArr = new ArrayList<Page>();
+		int numFrames= 1;
+		int algorithm =0;
 		
-				
 		
-		FileReader file	= new FileReader("gcc.trace");
+		FileReader file	= new FileReader("gccBig.trace");
 		Scanner sc = new Scanner(file);
-		
+		try {
 		Page thisPage;
 		while(sc.hasNext()){
 			thisPage = new Page();
@@ -31,14 +40,18 @@ public class Main {
 			}
 			pagesArr.add(thisPage);
 		}
+		}catch (Exception e){
+			System.out.println(pagesArr.size());
+		}
 		sc.close();
+		
 		//all data from file is into page table,
 		//now iterate over them like a simulation
 		
 		PageTable pageTable = new PageTable();
-		PageFrame pageFrame = new PageFrame(5,0);
+		PageFrame pageFrame = new PageFrame(numFrames,algorithm);
 		int pageTableIndex;
-		for(int i = 0;i< 100;i++){ //pagesArr.size()
+		for(int i = 0;i< pagesArr.size();i++){ 
 			//pageTable
 			pageTableIndex = pageTable.add(pagesArr.get(i)); //adds to page table, updates time
 			
@@ -56,8 +69,11 @@ public class Main {
 			
 		}
 		
-		
-		
+		System.out.println("Number of Frames: " +numFrames);
+		System.out.println("Number of Writes: " +PageTable.numWrites);
+		System.out.println("Number of Reads: " +PageTable.numReads);
+		System.out.println("Number of traces:" +pagesArr.size());
+		System.out.println("Not Page Fault successes: "+PageFrame.notPageFault );
 		
 		
 		
